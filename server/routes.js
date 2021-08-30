a5031643344e0fb3a9ffbd14fb2186f79a2959b4
mohit564 <86644125+mohit564@users.noreply.github.com>
2021-08-30 22:44:31 +0530

    feat: add recursive functionality for collecting user files

--- server/routes.js
+++ server/routes.js
@@ -1,6 +1,25 @@
 const express = require("express");
-const fs = require("fs");
+const fs = require("fs").promises;
 const path = require("path");
+
+// Recursively collect all files and directories information
+async function getFiles(folderName) {
+  let files = [];
+  const dirents = await fs.readdir(folderName, { withFileTypes: true });
+
+  for (const dirent of dirents) {
+    files.push({
+      name: dirent.name,
+      type: dirent.isDirectory() ? "folder" : "file",
+      ext: path.extname(dirent.name).slice(1),
+    });
+    if (dirent.isDirectory()) {
+      files = files.concat(await getFiles(path.join(folderName, dirent.name)));
+    }
+  }
+
+  return files;
+}
 
 // configure router
 const router = express.Router();
@@ -9,25 +28,16 @@ router.get("/", (req, res) => {
   res.send("Welcome to My Drive project API. Created by Mohit Dhule.");
 });
 
-router.get("/api/users/:user", (req, res) => {
-  const user = req.params.user;
-  const target = path.join(__dirname, "home", user);
-  const items = [];
+router.get("/api/users/:user", async (req, res) => {
+  const USER = req.params.user;
+  const USER_DIRECTORY = path.join(__dirname, "home", USER);
 
-  fs.readdir(target, { withFileTypes: true }, (err, data) => {
-    if (err) {
-      return res.status(404).json({ error: "User does not exist" });
-    }
-
-    data.forEach((item) =>
-      items.push({
-        name: item.name,
-        type: item.isDirectory() ? "folder" : "file",
-        ext: path.extname(item.name),
-      })
-    );
-    res.json(items);
-  });
+  try {
+    const files = await getFiles(USER_DIRECTORY);
+    res.json(files);
+  } catch {
+    res.json({ error: `User ${USER} not found` });
+  }
 });
 
 module.exports = router;
