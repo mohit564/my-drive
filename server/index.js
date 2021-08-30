f9faa3b31e992731917c53541cb6e5e01343b347
mohit564 <86644125+mohit564@users.noreply.github.com>
2021-08-30 22:28:44 +0530

    refactor: Rename variables and Reorganize directory

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const filesAPI = require("./filesAPI");
+const routes = require("./routes");
 
 const PORT = process.env.PORT || 5000;
 const NODE_ENV = process.env.NODE_ENV || "development";
@@ -12,7 +12,7 @@ app.set("env", NODE_ENV);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors({ origin: "*" }));
-app.use("/", filesAPI);
+app.use("/", routes);
 
 app.listen(PORT, (err) => {
   if (err) throw err;
